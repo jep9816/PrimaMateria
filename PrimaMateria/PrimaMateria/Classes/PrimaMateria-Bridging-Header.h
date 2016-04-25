bee00cd1af15aad8e2f6c9fe85265c2a7084fdae
Jerry Porter <jerry.porter@gulfstream.com>
2016-04-24 21:56:10 -0400

    updated

---
+++ PrimaMateria/PrimaMateria/Classes/PrimaMateria-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "CorePlot-CocoaTouch.h"
+#import "UIColor-Expanded.h"
+#import "UIColor-HSVAdditions.h"
+#import "MBProgressHUD.h"
