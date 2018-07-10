3328281bba2d2cdbb5e1d6370ffd25c40e05dd38
aramark-pivotal <aramark-pivotal@googlegroups.com>
2018-07-10 16:20:48 -0400

    updated html files with tidy and bumped version of app to 2.0.0

--- PrimaMateria/Resources/PrimaMateriaHelp/list.sh
+++ PrimaMateria/Resources/PrimaMateriaHelp/list.sh
@@ -1,5 +1,5 @@
-tidy GeneralInfo.html -m GeneralInfo.html
-tidy References.html -m References.html
-tidy ReleaseNotes.html -m ReleaseNotes.html
-tidy index.html -m index.html
-tidy License.html -m License.html
+tidy -i -asxml GeneralInfo.html -m GeneralInfo.html
+tidy -i -asxml References.html -m References.html
+tidy -i -asxml ReleaseNotes.html -m ReleaseNotes.html
+tidy -i -asxml index.html -m index.html
+tidy -i -asxml License.html -m License.html
